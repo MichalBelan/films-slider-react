9403f3cfdee7210acc63facaea1893b31ce3601b
Michal Belan <michalbelan503@gmail.com>
2023-10-06 17:00:37 +0200

    first commit

---
+++ src/components/OneMovieSlider.js
@@ -0,0 +1,65 @@
+import "./OneMovieSlider.css";
+import data from "../data";
+import { useState,useEffect } from "react";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+
+const OneMovieSlider = () => {
+
+const [index, setIndex] = useState(0)
+// Filmy se ve slideru točí stále dokola
+useEffect( () => {
+    if (index < 0){
+        setIndex(data.length - 1)
+    } else if (index > data.length - 1){
+        setIndex(0)
+    }
+}, [index])
+
+// Automatické posouvání
+useEffect( () => {
+    let setIntervalID = setInterval( () => {
+        setIndex(index + 1)
+    }, 3000)
+    return () => clearInterval(setIntervalID)
+}, [index])
+
+
+
+
+return <section className="all-movies">
+    <div className="all-movies-content">
+        {data.map( (oneMovie,oneMovieIndex) => {
+            const {id, image, title, tags, description } = oneMovie
+
+            let mainClass = "next-slide"
+
+
+            if (oneMovieIndex === index){
+                mainClass = "active-slide"
+            }
+
+
+            if (oneMovieIndex === index - 1 || (index === 0 && oneMovieIndex === data.length - 1)){
+                mainClass = "last-slide"
+
+            }
+
+            return <article key={id} className={mainClass}>
+                <img src={image} alt="" />
+                <h2>{title}</h2>
+                <p>{description}</p>        
+                <p>{tags}</p>
+                
+            </article>
+        })}
+    </div>
+    <button  onClick={ () => setIndex(index - 1)}>
+        <FaArrowAltCircleLeft />
+    </button>
+    <button onClick={ () => setIndex(index + 1)}>
+        <FaArrowAltCircleRight />
+    </button>
+</section>
+
+};
+export default OneMovieSlider;
